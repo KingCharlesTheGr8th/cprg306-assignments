278aaa68998d1c1e26e12f306c9691bf75281654
KingCharlesTheGr8th <119707706+KingCharlesTheGr8th@users.noreply.github.com>
2023-09-21 18:01:37 -0600

    Adds week 2 assignment

---
+++ app/studentinfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h2>Charles Ashmore</h2>
+            <p>CPRG 306B</p>
+            <p><Link href="https://github.com/KingCharlesTheGr8th">My Github</Link></p>
+        </div>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,20 @@
+import StudentInfo from "../studentinfo"
+import Link from "next/link";
+
+export default function Page() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between flex-col font-mono text-sm lg:flex">
+          <h1>My Shopping List!</h1>
+          <StudentInfo />
+          </div>
+          <div className="z-10 max-w-5xl w-full items-center justify-between flex-col font-mono text-sm lg:flex">
+            
+            </div>
+            <div>
+            <Link href="./">Back</Link>
+          </div>
+      </main>
+    )
+  }
+  
